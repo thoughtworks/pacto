d7e0f9c2ec173f4c64c502dd9ffbc906090063c2
Thiago Colucci and Tomas Mattia <abril_vejasp_dev+colucci+tomas@thoughtworks.com>
2013-02-04 15:45:17 -0200

    Refactored JSON::Generator::ObjectAttribute to inherit from BasicAttribute

--- lib/json/generator/object_attribute.rb
+++ lib/json/generator/object_attribute.rb
@@ -1,12 +1,8 @@
 module JSON
 	module Generator
-		class ObjectAttribute
-			def initialize(attributes)
-				@attributes = attributes
-			end
-
+		class ObjectAttribute < BasicAttribute
 			def generate
-				return nil unless @attributes['required']
+				return nil unless required?
 				return {} unless @attributes.has_key?('properties')
 				@attributes['properties'].inject({}) do |json, (property_name, property_attributes)|
 					json[property_name] = AttributeFactory.create(property_attributes).generate
