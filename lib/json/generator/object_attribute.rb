e5dfd93e46ba34cb66f43f3adf47a71b9a9e8bfc
Thiago Colucci and Tomas Mattia <abril_vejasp_dev+colucci+tomas@thoughtworks.com>
2013-02-04 15:04:57 -0200

    Reimplemented ObjectAttribute#generate to assume attributes are dereferenced

--- lib/json/generator/object_attribute.rb
+++ lib/json/generator/object_attribute.rb
@@ -1,6 +1,17 @@
 module JSON
 	module Generator
 		class ObjectAttribute
+			def initialize(attributes)
+				@attributes = attributes
+			end
+
+			def generate
+				return {} unless @attributes.has_key?('properties')
+				@attributes['properties'].inject({}) do |json, (property_name, property_attributes)|
+					json[property_name] = AttributeFactory.create(property_attributes).generate
+					json
+				end
+			end
 		end
 	end
 end
--- spec/json/generator/object_attribute_spec.rb
+++ spec/json/generator/object_attribute_spec.rb
@@ -1,6 +1,78 @@
 module JSON
 	module Generator
 		describe ObjectAttribute do
+			describe '#generate' do
+				context 'without properties' do
+					let(:attributes) { {'type' => 'object'} }
+
+					it 'should generate an empty object' do
+						described_class.new(attributes).generate.should == {}
+					end
+				end
+
+				context 'with empty properties' do
+					let(:attributes) { {'type' => 'object', 'properties' => {}} }
+
+					it 'should generate an empty object' do
+						described_class.new(attributes).generate.should == {}
+					end
+				end
+
+				context 'with one property' do
+					let(:attributes) do
+						{
+							'type' => 'object',
+							'properties' => {
+								'foo' => property_attributes
+							}
+						}
+					end
+					let(:property_attributes) { double('property attributes') }
+					let(:property) { double('property') }
+					let(:generated_property) { double('generated property') }
+
+					it 'should generate an object with one attribute' do
+						AttributeFactory.should_receive(:create).with(property_attributes).and_return(property)
+						property.should_receive(:generate).and_return(generated_property)
+
+						described_class.new(attributes).generate.should == {
+							'foo' => generated_property
+						}
+					end
+				end
+
+				context 'with many properties' do
+					let(:attributes) do
+						{
+							'type' => 'object',
+							'properties' => {
+								'foo' => foo_attributes,
+								'bar' => bar_attributes
+							}
+						}
+					end
+					let(:foo_attributes) { double('foo attributes') }
+					let(:foo_property) { double('foo property') }
+					let(:generated_foo) { double('generated foo') }
+
+					let(:bar_attributes) { double('bar attributes') }
+					let(:bar_property) { double('bar property') }
+					let(:generated_bar) { double('generated bar') }
+
+					it 'should generate an object with many attributes' do
+						AttributeFactory.should_receive(:create).with(foo_attributes).and_return(foo_property)
+						foo_property.should_receive(:generate).and_return(generated_foo)
+
+						AttributeFactory.should_receive(:create).with(bar_attributes).and_return(bar_property)
+						bar_property.should_receive(:generate).and_return(generated_bar)
+
+						described_class.new(attributes).generate.should == {
+							'foo' => generated_foo,
+							'bar' => generated_bar
+						}
+					end
+				end
+			end
 		end
 	end
 end
