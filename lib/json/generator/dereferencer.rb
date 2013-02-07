726b4489f03e1a99844f80edcbe6f692e6fe8552
Tomas Mattia <abril_vejasp_dev+tomas@thoughtworks.com>
2013-02-07 16:43:03 -0200

    Added basic implementation for JSON::Generator::Dereferencer.dereference

--- lib/json/generator/dereferencer.rb
+++ lib/json/generator/dereferencer.rb
@@ -2,6 +2,19 @@ module JSON
   module Generator
     class Dereferencer
       def self.dereference(schema)
+        return schema unless schema.has_key?('properties')
+
+        definitions = schema.delete('definitions')
+        schema['properties'].each do |name, property|
+          next unless property.has_key?('$ref')
+
+          ref_name = property['$ref'].split('/').last
+          raise NameError, "definition for #{ref_name} not found" unless definitions.has_key?(ref_name)
+
+          property.merge!(definitions[ref_name])
+          property.delete('$ref')
+        end
+
         schema
       end
     end
