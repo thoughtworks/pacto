3aa4ca67404a37d6d7042a6d268cd20d5bf728cb
Tomas Mattia <tomas.mattia@gmail.com>
2013-05-28 22:31:03 +0200

    Replaced json-generator code by gem

--- lib/json/generator/empty_attribute.rb
+++
@@ -1,7 +0,0 @@
-module JSON
-  module Generator
-    class EmptyAttribute < BasicAttribute
-      DEFAULT_VALUE = nil
-    end
-  end
-end
--- lib/json/generator/integer_attribute.rb
+++
@@ -1,7 +0,0 @@
-module JSON
-  module Generator
-    class IntegerAttribute < BasicAttribute
-      DEFAULT_VALUE = 0
-    end
-  end
-end
