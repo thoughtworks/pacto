a1deaae42a56d7b35e1b72c7587b3e037a554a36
Jesus Mercado <jesusmmm@gmail.com>
2013-09-21 01:28:58 -0300

    Remove contract unit test that is outdated

--- spec/unit/pacto/contract_spec.rb
+++ spec/unit/pacto/contract_spec.rb
@@ -9,29 +9,14 @@ module Pacto
       let(:instantiated_response) { double('instantiated response') }
       let(:instantiated_contract) { double('instantiated contract') }
 
-      context 'by default' do
-        it 'should instantiate a contract with default attributes' do
-          response.should_receive(:instantiate).and_return(instantiated_response)
-          InstantiatedContract.should_receive(:new).
-            with(request, instantiated_response).
-            and_return(instantiated_contract)
-          instantiated_contract.should_not_receive(:replace!)
+      it 'should instantiate a contract with default attributes' do
+        response.should_receive(:instantiate).and_return(instantiated_response)
+        InstantiatedContract.should_receive(:new).
+          with(request, instantiated_response).
+          and_return(instantiated_contract)
+        instantiated_contract.should_not_receive(:replace!)
 
-          contract.instantiate.should == instantiated_contract
-        end
-      end
-
-      context 'with extra attributes' do
-        let(:attributes) { {:foo => 'bar'} }
-
-        it 'should instantiate a contract and overwrite default attributes' do
-          response.should_receive(:instantiate).and_return(instantiated_response)
-          InstantiatedContract.should_receive(:new).
-            with(request, instantiated_response).
-            and_return(instantiated_contract)
-
-          contract.instantiate.should == instantiated_contract
-        end
+        contract.instantiate.should == instantiated_contract
       end
     end
 
