404a4aafa01c8fbad7c06050ea759dd5f8960974
Max Lincoln <max@devopsy.com>
2013-09-25 14:38:53 -0300

    something in e2e is not leaking data across tests.  use :all for now, but hope to find the root cause

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,7 @@ require 'pacto/server'
 require 'stringio'
 
 RSpec.configure do |config|
+  # I'd like this to be before :each, but there is an issue with one test
   config.before(:each) do
     Pacto.clear!
   end
