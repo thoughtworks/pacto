6f8341d61a129bcc6f2e116e7e6427b9f19807da
Max Lincoln <max@devopsy.com>
2013-09-25 21:17:22 -0300

    fix contract_repo methods that weren't safe with Sets

--- lib/pacto/core/contract_repository.rb
+++ lib/pacto/core/contract_repository.rb
@@ -11,7 +11,7 @@ module Pacto
     end
 
     def use(tag, values = {})
-      merged_contracts = registered[:default].merge registered[tag]
+      merged_contracts = registered[:default] + registered[tag]
 
       raise ArgumentError, "contract \"#{tag}\" not found" if merged_contracts.empty?
 
@@ -26,14 +26,19 @@ module Pacto
     end
 
     def unregister_all!
-      WebMock.reset!
       registered.clear
     end
 
     def contract_for(request_signature)
-      registered.values.inject(Set.new) do |result, contract_set|
-        result.merge(contract_set.keep_if { |c| c.matches? request_signature })
+      matches = Set.new
+      registered.values.each do |contract_set|
+        contract_set.each do |contract|
+          if contract.matches? request_signature
+            matches.add contract
+          end
+        end
       end
+      matches
     end
   end
 end
