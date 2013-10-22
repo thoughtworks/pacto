b99cf81c83d094f3643f5d6e242d6eccf7df03b4
Jesús Mercado <jesusmmm@gmail.com>
2013-10-22 01:45:34 -0200

    Add one more assertion to the unit test

--- spec/unit/pacto/contract_factory_spec.rb
+++ spec/unit/pacto/contract_factory_spec.rb
@@ -2,7 +2,8 @@ module Pacto
   describe ContractFactory do
     let(:host)               { 'http://localhost' }
     let(:contract_name)      { 'contract' }
-    let(:contract_path)      { File.join('spec', 'unit', 'data', "#{contract_name}.json") }
+    let(:contracts_path)     { ['spec', 'unit', 'data'] }
+    let(:contract_path)      { File.join(contracts_path, "#{contract_name}.json") }
     let(:file_pre_processor) { double('file_pre_processor') }
     let(:file_content)       { File.read(contract_path) }
 
@@ -19,11 +20,14 @@ module Pacto
     end
 
     describe '.load' do
-      let(:contract) { double 'contract' }
+      let(:contract) { double :contract }
+
       it 'builds a contract from a relative path' do
-        Pacto.configuration.contracts_path = 'my_contracts'
-        described_class.should_receive(:build_from_file).with('my_contracts/the_contract.json', host).and_return(contract)
-        expect(described_class.load('the_contract', host)).to eq(contract)
+        # TODO: We should not stub a public method of the SUT. This is a smell
+        # of this class having more than one responsibility
+        Pacto.configuration.should_receive(:contracts_path).and_return contracts_path
+        described_class.should_receive(:build_from_file).with(contract_path, host, nil).and_return(contract)
+        expect(described_class.load(contract_name, host)).to eq(contract)
       end
     end
   end
