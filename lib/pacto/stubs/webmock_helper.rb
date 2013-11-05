815bcd602c969332c0e30d0c0656e23c7c8e959d
Jesús Mercado <jesusmmm@gmail.com>
2013-11-04 23:33:08 -0200

    Normalize the use of the logger (as in the whole code)

--- lib/pacto/stubs/webmock_helper.rb
+++ lib/pacto/stubs/webmock_helper.rb
@@ -2,17 +2,15 @@ module Pacto
   module Stubs
     class WebMockHelper
       class << self
-        LOGGER = Logger.instance
-
         def validate(request_signature, response)
           pacto_response = webmock_to_pacto_response(response)
           contract = Pacto.contract_for(request_signature)
-          Logger.instance.debug("Validating #{request_signature}, #{response} against #{contract}")
+          logger.debug("Validating #{request_signature}, #{response} against #{contract}")
           puts contract.validate(pacto_response) unless contract.nil?
         end
 
         def generate(request_signature, response)
-          LOGGER.debug("Generating Contract for #{request_signature}, #{response}")
+          logger.debug("Generating Contract for #{request_signature}, #{response}")
           uri = URI(request_signature.uri)
           basename = File.basename(uri.path, '.json') + '.json'
           pacto_request = webmock_to_pacto_request(request_signature)
@@ -25,14 +23,18 @@ module Pacto
             FileUtils.mkdir_p(File.dirname contract_file)
             begin
               File.write(contract_file, generator.save('vcr', pacto_request, pacto_response))
-              LOGGER.debug("Generating #{contract_file}")
+              logger.debug("Generating #{contract_file}")
             rescue => e
-              LOGGER.error("Error while generating Contract: #{e.inspect}")
+              logger.error("Error while generating Contract: #{e.inspect}")
             end
           end
         end
 
         private
+
+        def logger
+          @logger ||= Logger.instance
+        end
 
         def webmock_to_pacto_request webmock_request
           uri = URI(webmock_request.uri)
