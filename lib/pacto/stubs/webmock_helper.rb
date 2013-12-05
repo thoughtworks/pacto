497106618eeab84d6afb9271482cb8cfc12d2461
Max Lincoln <max@devopsy.com>
2013-12-05 18:47:50 -0300

    load on generate (so validations work immediately) and log errors

--- lib/pacto/stubs/webmock_helper.rb
+++ lib/pacto/stubs/webmock_helper.rb
@@ -14,23 +14,25 @@ module Pacto
           return if skip? request_signature
 
           logger.debug("Generating Contract for #{request_signature}, #{response}")
-          uri = URI(request_signature.uri)
-          basename = File.basename(uri.path, '.json') + '.json'
-          pacto_request = webmock_to_pacto_request(request_signature)
-          pacto_response = webmock_to_pacto_response(response)
-          # contract_file = File.expand_path(Pacto.configuration.contracts_path, File.dirname(uri.path), basename)
-          contract_file = File.join(Pacto.configuration.contracts_path, uri.host, File.dirname(uri.path), basename)
+          begin
+            uri = URI(request_signature.uri)
+            basename = File.basename(uri.path, '.json') + '.json'
+            pacto_request = webmock_to_pacto_request(request_signature)
+            pacto_response = webmock_to_pacto_response(response)
+            # contract_file = File.expand_path(Pacto.configuration.contracts_path, File.dirname(uri.path), basename)
+            contract_file = File.join(Pacto.configuration.contracts_path, uri.host, File.dirname(uri.path), basename)
 
-          unless File.exists? contract_file
-            generator = Pacto::Generator.new
-            FileUtils.mkdir_p(File.dirname contract_file)
-            begin
+            unless File.exists? contract_file
+              generator = Pacto::Generator.new
+              FileUtils.mkdir_p(File.dirname contract_file)
               File.write(contract_file, generator.save('vcr', pacto_request, pacto_response))
               logger.debug("Generating #{contract_file}")
-            rescue => e
-              logger.error("Error while generating Contract #{contract_file}: #{e}")
-              logger.error("Backtrace: #{e.backtrace}")
+
+              Pacto.load contract_file, uri.host, :generated
             end
+          rescue => e
+            logger.error("Error while generating Contract #{contract_file}: #{e}")
+            logger.error("Backtrace: #{e.backtrace}")
           end
         end
 
