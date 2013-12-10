add23ca454efc786849b860a11c1af1a41bc4cdb
Max Lincoln <max@devopsy.com>
2013-12-10 00:41:34 -0300

    add ContractValidator to move logic out of Contract and make it easier to test

---
+++ lib/pacto/contract_validator.rb
@@ -0,0 +1,28 @@
+module Pacto
+  class ContractValidator
+    class << self
+      def validate contract, request, response, opts
+        env = {
+          :contract => contract,
+          :actual_request => request,
+          :actual_response => response,
+          :validation_results => []
+        }
+        validation_stack(opts).call env
+        env[:validation_results].compact
+      end
+
+      private
+
+      def validation_stack opts
+        Middleware::Builder.new do
+          use Pacto::Validators::ResponseBodyValidator
+          unless opts[:body_only]
+            use Pacto::Validators::ResponseStatusValidator
+            use Pacto::Validators::ResponseHeaderValidator
+          end
+        end
+      end
+    end
+  end
+end
