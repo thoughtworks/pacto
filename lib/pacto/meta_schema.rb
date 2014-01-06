b49780a106ca87b955653424764c7eafc21c1ed5
Marcos Matos <marcosccm@gmail.com>
2014-01-06 11:03:19 -0200

    JSON::Validator.cache_schemas is deprecated

--- lib/pacto/meta_schema.rb
+++ lib/pacto/meta_schema.rb
@@ -5,7 +5,6 @@ module Pacto
     def initialize(engine = JSON::Validator)
       @schema = File.join(File.dirname(File.expand_path(__FILE__)), '../../resources/contract_schema.json')
       @base_schema = File.join(File.dirname(File.expand_path(__FILE__)), '../../resources/draft-03.json')
-      JSON::Validator.cache_schemas = true
       JSON::Validator.validate!(@base_schema, @schema)
       @engine = engine
     end
