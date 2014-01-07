56335b2e93ff8f1ad1f0d8084f85f311acb2c50b
Marcos Matos <marcosccm@gmail.com>
2014-01-07 10:32:47 -0200

    Compare strings instead of regexes

--- spec/unit/pacto/stubs/uri_pattern_spec.rb
+++ spec/unit/pacto/stubs/uri_pattern_spec.rb
@@ -6,14 +6,14 @@ module Pacto
       it 'returns a regex containing the host and path' do
         Pacto.configuration.strict_matchers = false
         request = double(host: 'myhost.com', path: '/stuff')
-        expect(UriPattern.for(request)).to eq(/myhost\.com\/stuff/)
+        expect(UriPattern.for(request).to_s).to eq(/myhost\.com\/stuff/.to_s)
       end
 
       it 'turns segments preceded by : into wildcards' do
         Pacto.configuration.strict_matchers = false
         request = double(host: 'myhost.com', path: '/:id')
         wildcard = '[^\/\?#]+'
-        expect(UriPattern.for(request)).to eq(/myhost\.com\/#{wildcard}/)
+        expect(UriPattern.for(request).to_s).to eq(/myhost\.com\/#{wildcard}/.to_s)
       end
     end
 
