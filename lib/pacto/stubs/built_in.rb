f4ae3e85410ca2aeb8f90f798f7d0e4f112acfc4
Marcos Matos <marcosccm@gmail.com>
2014-01-08 11:30:18 -0200

    BuiltIn doesn't need to return the pattern

--- lib/pacto/stubs/built_in.rb
+++ lib/pacto/stubs/built_in.rb
@@ -9,9 +9,16 @@ module Pacto
       end
 
       def stub_request!(request, response)
-        request_pattern = build_request_pattern(request, response)
-        request_pattern.with(request_details(request)) if Pacto.configuration.strict_matchers
-        request_pattern
+        uri_pattern = UriPattern.for(request)
+        stub = WebMock.stub_request(request.method, uri_pattern)
+
+        stub.request_pattern.with(strict_details(request)) if Pacto.configuration.strict_matchers
+
+        stub.to_return(
+          :status => response.status,
+          :headers => response.headers,
+          :body => format_body(response.body)
+        )
       end
 
       def reset!
@@ -30,17 +37,6 @@ module Pacto
 
       private
 
-      def build_request_pattern(request, response)
-        uri_pattern = UriPattern.for(request)
-        stub = WebMock.stub_request(request.method, uri_pattern)
-        stub.to_return(
-          :status => response.status,
-          :headers => response.headers,
-          :body => format_body(response.body)
-        )
-        stub.request_pattern
-      end
-
       def register_hooks
         WebMock.after_request do |request_signature, response|
           process_hooks request_signature, response
@@ -55,15 +51,15 @@ module Pacto
         end
       end
 
-      def request_details(request)
-        details = {}
-        unless request.params.empty?
-          details[webmock_params_key(request)] = request.params
+      def strict_details(request)
+        {}.tap do |details|
+          unless request.params.empty?
+            details[webmock_params_key(request)] = request.params
+          end
+          unless request.headers.empty?
+            details[:headers] = request.headers
+          end
         end
-        unless request.headers.empty?
-          details[:headers] = request.headers
-        end
-        details
       end
 
       def webmock_params_key(request)
