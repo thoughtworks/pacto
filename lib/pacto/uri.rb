1a4518b628bdb8db5e64d9685d09b4ddb4a175e2
Marcos Matos <marcosccm@gmail.com>
2014-02-11 11:55:00 -0200

    Add scheme normalization to Pacto:URI

--- lib/pacto/uri.rb
+++ lib/pacto/uri.rb
@@ -5,11 +5,24 @@ module Pacto
       @path = path
     end
 
+    def uri
+      if base_uri.scheme.nil?
+        Addressable::URI.parse "http://#{base_uri}"
+      else
+        base_uri
+      end
+    end
+
+    def base_uri
+      Addressable::URI.parse("#{host}#{path}")
+    end
+
     def to_s
-      "#{host}#{path}"
+      uri.to_s
     end
 
     private
+
     attr_reader :host, :path
   end
 end
--- spec/unit/pacto/uri_spec.rb
+++ spec/unit/pacto/uri_spec.rb
@@ -6,5 +6,10 @@ module Pacto
       uri = URI.new("https://localhost", "/bla")
       expect(uri.to_s).to eq "https://localhost/bla"
     end
+
+    it 'uses http as the default scheme for hosts' do
+      uri = URI.new("localhost", "/bla")
+      expect(uri.to_s).to eq "http://localhost/bla"
+    end
   end
 end
