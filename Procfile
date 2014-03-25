14ded84aa4630e9aaa3ca5cbe82503fc13c7e16d
Max Lincoln <max@devopsy.com>
2014-03-24 21:39:51 -0400

    Remove unnecessary changes

--- Procfile
+++
@@ -1,2 +0,0 @@
-sample_apis: sh -c 'cd samples/sample_apis && bundle exec puma config.ru'
-guard:       bundle exec guard
--- samples/validate.rb
+++
@@ -1,9 +0,0 @@
-require 'json-schema'
-require 'json'
-
-data = JSON.parse(File.read 'result.json')
-schema = JSON.parse(File.read 'schema.json')
-# data = JSON.parse(File.read 'schema.json')
-# schema = JSON.parse(File.read 'draft3.json')
-
-puts JSON::Validator.fully_validate schema, data, :version => :draft3, :validate_schema => true
