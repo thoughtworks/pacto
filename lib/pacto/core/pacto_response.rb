fafa6f1d9e91fbcf6a4341a39e212802bad1f2c7
Max Lincoln <max@devopsy.com>
2014-06-04 18:43:38 -0400

    (Actually fix it this time... didn't realize I'd rbenv'ed back to Ruby 2)

--- lib/pacto/core/pacto_response.rb
+++ lib/pacto/core/pacto_response.rb
@@ -1,11 +1,11 @@
 module Pacto
   class PactoResponse
     # FIXME: Need case insensitive header lookup, but case-sensitive storage
-    attr_accessor :body, :headers, :status
+    attr_accessor :headers, :body, :status
 
     def initialize(data)
       mash = Hashie::Mash.new data
-      @headers = mash.headers.nil? ? {} : mash.headers.to_h
+      @headers = mash.headers.nil? ? {} : mash.headers
       @body    = mash.body
       @status  = mash.status.to_i
     end
