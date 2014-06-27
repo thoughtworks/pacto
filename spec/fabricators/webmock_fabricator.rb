3d3b2890e0162df2a73b53d38640107d562696ea
Max Lincoln <max@devopsy.com>
2014-06-26 23:08:31 -0400

    Rubocop 0.24.0'ed

--- spec/fabricators/webmock_fabricator.rb
+++ spec/fabricators/webmock_fabricator.rb
@@ -4,7 +4,7 @@ Fabricator(:webmock_request_signature, from: WebMock::RequestSignature) do
   initialize_with do
     uri = _transient_attributes[:uri]
     method = _transient_attributes[:method]
-    uri = Addressable::URI.heuristic_parse(uri) unless uri.kind_of? Addressable::URI
+    uri = Addressable::URI.heuristic_parse(uri) unless uri.is_a? Addressable::URI
     WebMock::RequestSignature.new method, uri
   end
   transient method: :get
@@ -15,7 +15,7 @@ Fabricator(:webmock_request_pattern, from: WebMock::RequestPattern) do
   initialize_with do
     uri = _transient_attributes[:uri]
     method = _transient_attributes[:method]
-    uri = Addressable::URI.heuristic_parse(uri) unless uri.kind_of? Addressable::URI
+    uri = Addressable::URI.heuristic_parse(uri) unless uri.is_a? Addressable::URI
     WebMock::RequestPattern.new method, uri
   end
   transient method: :get
